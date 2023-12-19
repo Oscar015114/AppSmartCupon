6af9ef99615ee01b21525a0cf53607cc39a2eb4a
Oscar015114 <pereag30@gmail.com>
2023-12-18 22:07:22 -0600

    Se creo el poko y se agrego el cliente ,

---
+++ app/src/main/java/com/example/appsmartcupon/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.appsmartcupon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appsmartcupon.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+}
---
+++ app/src/main/java/com/example/appsmartcupon/poko/Cliente.kt
@@ -0,0 +1,15 @@
+package com.example.appsmartcupon.poko
+
+class Cliente {
+    var idCliente: Int = 0
+    var nombre: String =""
+    var apellidoPaterno: String =""
+    var apellidoMaterno: String =""
+    var telefono: String=""
+    var correo: String=""
+    var calle: String= ""
+    var numero: Int = 0
+    var contrasenia: String=""
+    var fechaNacimiento: String=""
+
+}
