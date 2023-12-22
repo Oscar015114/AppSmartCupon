30dcf12533fc68513ea510bbedf7fab655848f4e
Oscar015114 <pereag30@gmail.com>
2023-12-22 09:27:49 -0600

    se corrigieron los ids y en homeactivity creamos la funcion de los botones

--- app/src/main/java/com/example/appsmartcupon/HomeActivity.kt
+++ app/src/main/java/com/example/appsmartcupon/HomeActivity.kt
@@ -1,11 +1,32 @@
 package com.example.appsmartcupon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appsmartcupon.databinding.ActivityHomeBinding
+import com.example.appsmartcupon.poko.Cliente
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnCliente.setOnClickListener{
+            val irCliente = Intent(this@HomeActivity, EditarCuentaActivity::class.java)
+            startActivity(irCliente)
+            finish()
+        }
+
+        binding.btnCategorias.setOnClickListener{
+            val irCategorias = Intent(this@HomeActivity, CategoriasActivity::class.java)
+            startActivity(irCategorias)
+            finish()
+        }
     }
 }
