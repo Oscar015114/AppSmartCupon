9f7509674e1954b99cb05f52949e47576742cd12
Oscar015114 <pereag30@gmail.com>
2023-12-26 17:18:47 -0600

    creacion detallesPromocion, creacion categorias

--- app/src/main/java/com/example/appsmartcupon/poko/Categoria.java
+++
@@ -1,4 +0,0 @@
-package com.example.appsmartcupon.poko;
-
-public class Categoria {
-}
---
+++ app/src/main/java/com/example/appsmartcupon/poko/Categoria.kt
@@ -0,0 +1,6 @@
+package com.example.appsmartcupon.poko
+
+class Categoria {
+    var idCategoria: Int = 0
+    var categoria: String = ""
+}
