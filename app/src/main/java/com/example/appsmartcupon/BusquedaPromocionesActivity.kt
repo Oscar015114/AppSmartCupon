05d17824b3f36c082f8ea8f6e3bb181f39f7606c
Oscar015114 <pereag30@gmail.com>
2023-12-26 20:11:47 -0600

    se creo activity busqueda promociones

---
+++ app/src/main/java/com/example/appsmartcupon/BusquedaPromocionesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appsmartcupon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BusquedaPromocionesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_busqueda_promociones)
+    }
+}
