2d8f40e67e4ec981a4cd7b220e02eaed2fb2d6ce
Christian <filetelopezchristian@gmail.com>
2023-12-31 15:41:39 -0600

    Cambios no importantes

--- app/src/main/java/com/example/appsmartcupon/util/Constantes.kt
+++ app/src/main/java/com/example/appsmartcupon/util/Constantes.kt
@@ -2,7 +2,7 @@ package com.example.appsmartcupon.util
 
 object Constantes {
 
-    const val URL_WS : String = "http://192.168.1.77:8084/APICuponSmart/api/"
+    const val URL_WS : String = "http://192.168.0.108:8084/APICuponSmart/api/"
     const val CAMPOS_OBLIGATORIOS: String = "Campo invalido"
     const val PROMOCION_DESCUENTO: String = "descuento"
     const val PROMOCION_REBAJADO: String = "rebajado"
